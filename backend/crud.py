12f4dabe8154bc4a9f290ea240efdccc00bf1d18
Sorrger <Sorrger@gmail.com>
2025-03-06 21:00:08 +0100

    cruds and prudcts route begin

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,31 @@
 from sqlalchemy.orm import Session
 from models import Product, Producer
 from schemas import CreateProduct, CreateProducer
+
+def get_products(db: Session):
+    return db.query(Product).all()
+
+def get_product_by_id(db: Session, product_id: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if db_product:
+        return db_product
+    else:
+        raise ValueError(f"Product with id {product_id} does not exist.")
+
+def add_product(db: Session, product: CreateProduct):
+    db_product = Product(name=product.name, photo_url=product.photo_url,
+                         description=product.description, price=product.price,
+                          producer_id= product.producer_id )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def remove_product(db: Session, product_id: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+        return db_product
+    else:
+        raise ValueError(f"Product with id {product_id} does not exist.")
---
+++ backend/routes/products.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from crud import get_products, get_product_by_id, add_product, remove_product
+from schemas import CreateProducer, CreateProduct
+
+router = APIRouter()
+
+@router.get("/products", response_model=list[CreateProduct])
+async def get_all_products(db: Session = Depends(get_db)):
+    products = get_products(db)
+    return products
