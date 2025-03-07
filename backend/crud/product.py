afb5658adfbf2e51824f9d6a13d155f2373f51f9
Sorrger <Sorrger@gmail.com>
2025-03-07 15:02:03 +0100

    crud refactor strcuture

--- backend/crud/product.py
+++ backend/crud/product.py
@@ -1,7 +1,18 @@
 from sqlalchemy.orm import Session
-from models import Product, Producer
-from schemas import CreateProduct, CreateProducer
+from models import Product
+from schemas import CreateProduct
 
+#create
+def add_product(db: Session, product: CreateProduct):
+    db_product = Product(name=product.name, photo_url=product.photo_url,
+                         description=product.description, price=product.price,
+                          producer_id= product.producer_id )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+#read
 def get_products(db: Session):
     return db.query(Product).all()
 
@@ -12,15 +23,9 @@ def get_product_by_id(db: Session, product_id: int):
     else:
         raise ValueError(f"Product with id {product_id} does not exist.")
 
-def add_product(db: Session, product: CreateProduct):
-    db_product = Product(name=product.name, photo_url=product.photo_url,
-                         description=product.description, price=product.price,
-                          producer_id= product.producer_id )
-    db.add(db_product)
-    db.commit()
-    db.refresh(db_product)
-    return db_product
+#update
 
+#delete
 def remove_product(db: Session, product_id: int):
     db_product = db.query(Product).filter(Product.id == product_id).first()
     if db_product:
