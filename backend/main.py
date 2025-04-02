e28072050f51e625c81529c85aeba42adb7c5d8b
Sorrger <Sorrger@gmail.com>
2025-04-02 13:39:22 +0200

    configuration of connection between react and fastapi

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,13 @@ from database import init_db
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"], #here http://localhost:PORT adres of frontend later
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], #here http://localhost:PORT adres of frontend later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(products.router)
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,48 @@
 import viteLogo from '/vite.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/products");
+        setProducts(response.data);
+      } catch (err) {
+        setError(`Error fetching products: ${err.message}`);
+        console.error("Axios Error:", err);
+      }
+      
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
+      <h1>Product List</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <ul>
-        <li>lista 1</li>
-        <li>lista 2</li>
-        <li>lista 3</li>
-        <li>lista 4</li>
-        <li>lista 5</li>
+        {products.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong>
+            <p>{product.description}</p>
+            {product.specifications && (
+              <ul>
+                {Object.entries(product.specifications).map(([key, value]) => (
+                  <li key={key}>
+                    <strong>{key}:</strong> {value}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
       </ul>
     </>
   );
